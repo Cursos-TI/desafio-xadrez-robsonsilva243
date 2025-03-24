ecf1aeeb3659d66b4c9f16d5bb32cfa218822218
Robson Silva <robsonsilva243@gmail.com>
2025-03-24 02:05:30 +0000

    Introduzindo a Movimentacao do Cavalo

--- xadrez.c
+++ xadrez.c
@@ -5,11 +5,13 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    
+
     //Definindo o numero de casa a serem movidas para cada pecas
     const int torre = 5;
     const int rainha = 8;
     const int bispo = 5;
+    const int cavaloBaixo = 2; // O Cavalo se move 2 casas para baixo
+    const int cavaloEsquerda = 1; // O Cavalo se move 1 casa para a esquerda
 
     //Menu Interativo
     printf("Xadrez\n");
@@ -39,6 +41,22 @@ int main() {
         printf("cima, direta\n");
         k++;
     } while (k < bispo);
+
+    // Separando a movimentação do Cavalo com uma linha em branco
+    printf("\n");
+
+    // Movimentação do Cavalo, 2 casas para baixo e 1 casa para a esquerda
+    printf("Cavalo\n");
+    for (int i = 0; i < cavaloBaixo; i++) {
+        printf("Baixo\n");
+    }
+
+    // Usando um loop while para simular o movimento para a esquerda
+    int l = 0;
+    while (l < cavaloEsquerda) {
+        printf("Esquerda\n");
+        l++;
+    }
     
 
 
